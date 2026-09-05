3780680ee27acb3ffeb72e6c43445effc014bb2d
andrewortiz-pixel <andrew.ortiz@tecsup.edu.pe>
2026-09-04 19:27:16 -0500

    feat: Add initial project structure and Application class

---
+++ src/main/java/pe/edu/tecsup/lab03/Application.java
@@ -0,0 +1,10 @@
+package pe.edu.tecsup.lab03;
+
+public class Application {
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        System.out.println("Inicializando la aplicación CPS Lab03...");
+    }
+
+}
